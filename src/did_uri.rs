c6962d15f64a273eacc14046c433682ea34fb9e4
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2023-11-08 17:32:39 -0500

    Fix clippy warnings
    Co-authored-by: Mario Alejandro Rodríguez <MarioRodriguezS@users.noreply.github.com>

--- src/did_uri.rs
+++ src/did_uri.rs
@@ -16,7 +16,7 @@ fn concat_did_uri(e: &Env, did_method: &String, method_specific_id: &[u8]) -> St
 
     let did_method_len = did_method.len() as usize;
 
-    let msi_len = method_specific_id.len() as usize;
+    let msi_len = method_specific_id.len();
 
     let mut slice = [0u8; 100]; // should be big enough for the DID URI
     prefix.copy_into_slice(&mut slice[..prefix_len]);
@@ -26,7 +26,7 @@ fn concat_did_uri(e: &Env, did_method: &String, method_specific_id: &[u8]) -> St
     let did_uri_len = prefix_len + did_method_len + 1;
 
     let mut msi_bytes = Bytes::new(e);
-    msi_bytes.extend_from_slice(&method_specific_id);
+    msi_bytes.extend_from_slice(method_specific_id);
 
     msi_bytes.copy_into_slice(&mut slice[did_uri_len..did_uri_len + msi_len]);
     let str_did_uri = core::str::from_utf8(slice[..did_uri_len + msi_len].as_ref()).unwrap();
@@ -37,8 +37,8 @@ fn concat_did_uri(e: &Env, did_method: &String, method_specific_id: &[u8]) -> St
 fn get_random_bytes(e: &Env) -> [u8; 15] {
     let mut random_bytes = [0u8; 15];
 
-    for i in 0..15 {
-        random_bytes[i] = e.prng().u64_in_range(0..256) as u8;
+    for byte in &mut random_bytes {
+        *byte = e.prng().u64_in_range(0..256) as u8;
     }
 
     random_bytes
