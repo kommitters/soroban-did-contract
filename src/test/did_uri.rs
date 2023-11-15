708615e6aa03bb14ce3537591ee2cb402da214a8
Miguel Nieto <39246879+miguelnietoa@users.noreply.github.com>
2023-11-15 11:56:18 -0500

    Add test suite
    Co-authored-by: Mario Alejandro Rodríguez <MarioRodriguezS@users.noreply.github.com>

--- src/test/did_uri.rs
+++ src/test/did_uri.rs
@@ -2,7 +2,7 @@ use crate::did_uri;
 use soroban_sdk::{ Env, String };
 
 #[test]
-fn test_concat_field_id() {
+fn test_concat_fragment() {
     let e: Env = Default::default();
     let did_uri = String::from_slice(&e, "did:chaincerts:zEYJrMxWigf9boyeJMTRN4Ern8DJMoCXaLK77pzQmxVjf");
     let id = String::from_slice(&e, "chaincerts");
